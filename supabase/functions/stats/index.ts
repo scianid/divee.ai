b412e95a6d380ee2f50c9391b72e4b46500af2e4
Moshe <moshe.maman@gmail.com>
2026-01-16 22:49:37 +0000

    fix: remove duplicate CORS header declaration

--- supabase/functions/stats/index.ts
+++ supabase/functions/stats/index.ts
@@ -6,8 +6,6 @@ const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 };
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
-};
 
 interface StatsParams {
   accountId?: string;
