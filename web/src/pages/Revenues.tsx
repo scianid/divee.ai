eee0b5cc9f1231dac1a4495ef101a0caed03c256
Moshe <moshe.maman@gmail.com>
2026-02-15 21:56:23 +0000

    feat: remove unused _DollarIcon component from Revenues page

--- web/src/pages/Revenues.tsx
+++ web/src/pages/Revenues.tsx
@@ -71,13 +71,6 @@ const CalendarIcon = () => (
   </svg>
 );
 
-const _DollarIcon = () => (
-  <svg width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-    <line x1="12" y1="1" x2="12" y2="23"></line>
-    <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
-  </svg>
-);
-
 // --- Helper Components ---
 
 function Card({ 
